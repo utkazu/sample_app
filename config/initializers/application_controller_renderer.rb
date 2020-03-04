e3e70091c02dbc196281a0d37f985d80fe91e39e
utkazu <hiroshimazeal@gmail.com>
2020-03-05 00:38:17 +0900

    [Finish] 3-8 couldn't solve error

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd68cf218fe90238db142e771a3279c795b80a8c8c4101a2ed9632a825d04f45ddd9944f0a16305f92bf8ce87dde78498eb17d78b2773b89ee5e3d85c4f32a883'
